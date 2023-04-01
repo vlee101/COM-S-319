026f32752862d33f66ec107799038612bbaddf91
HappyGiraffe17 <giraffe4dev@gmail.com>
2023-04-01 18:55:33 -0500

    Fix bug that breaks the site when reloading.

--- Assignment 2/assignment2/src/App.js
+++ Assignment 2/assignment2/src/App.js
@@ -376,39 +376,42 @@ export function App() {
   //displayBrowsePage(Products);
 }
 
-$(".like-btn").on("click", function () {
-  $(this).toggleClass("is-active");
-});
+$(window).on('load', () => {
+  $(".like-btn").on("click", function () {
+    $(this).toggleClass("is-active");
+  });
 
-$(".minus-btn").on("click", function (e) {
-  e.preventDefault();
-  var $this = $(this);
-  var $input = $this.closest("div").find("input");
-  var value = parseInt($input.val());
+  $(".minus-btn").on("click", function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest("div").find("input");
+    var value = parseInt($input.val());
 
-  if (value > 1) {
-    value = value - 1;
-  } else {
-    value = 0;
-  }
+    if (value > 1) {
+      value = value - 1;
+    } else {
+      value = 0;
+    }
 
-  $input.val(value);
-});
+    $input.val(value);
+  });
 
-$(".plus-btn").on("click", function (e) {
-  e.preventDefault();
-  var $this = $(this);
-  var $input = $this.closest("div").find("input");
-  var value = parseInt($input.val());
+  $(".plus-btn").on("click", function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest("div").find("input");
+    var value = parseInt($input.val());
 
-  if (value < 100) {
-    value = value + 1;
-  } else {
-    value = 100;
-  }
+    if (value < 100) {
+      value = value + 1;
+    } else {
+      value = 100;
+    }
 
-  $input.val(value);
-});
+    $input.val(value);
+  });
+}
+);
 
 // function Browse() {
 
