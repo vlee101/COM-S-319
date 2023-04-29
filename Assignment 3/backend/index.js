d73f380c705999a35fa6c6299aab9d20138240b9
Vicky Lee <vlee10101@gmail.com>
2023-04-29 11:43:04 -0500

    I did something

--- Assignment 3/backend/index.js
+++ Assignment 3/backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Product = require("./14_2_21_dataSchema.js");
+const Product = require("./dataSchema.js");
 const app = express();
 app.use(express.json());
 app.use(cors());
